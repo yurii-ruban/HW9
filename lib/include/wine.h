5f16d8642500d06323fc2af8ed4fc5779899bd05
Yurii Ruban <yurii.ruban@volvocars.com>
2020-06-30 02:45:50 +0300

    Implement the first approach

--- lib/include/wine.h
+++ lib/include/wine.h
@@ -1,10 +1,35 @@
 #ifndef _WINE_H
 #define _WINE_H
 
+#include <valarray>
+#include <cstring>
+#include <utility>
+#include <string>
+#include <time.h>
+#include <iostream>
+
+using ArrayIntT = std::valarray<int>;
+using PairArrayT = std::pair<ArrayIntT, ArrayIntT>;
+
+
 #ifdef USE_COMPOSITION
     class Wine
     {
+        public:
+            Wine(const char* l, int y, const int yr[], const int bot[]);
+            Wine(const char* l, int y);
+            ~Wine();
+            void getBottles();
+            void show() const;
+            const char* label() const;
+            size_t sum() const;
         
+        private:
+            char* name;
+            PairArrayT bottlingInfo;
+            ArrayIntT ages;
+
+            void calculateAges();          
     };
 #else
     class Wine
--- lib/src/wine.cpp
+++ lib/src/wine.cpp
@@ -1,13 +1,82 @@
 #include "wine.h"
 
 #ifdef USE_COMPOSITION
-    class Wine
+    Wine::Wine(const char* l, int y, const int yr[], const int bot[]) : name(new char[strlen(l)]),
+                                                                        bottlingInfo(std::make_pair(ArrayIntT(yr, y), ArrayIntT(bot, y))),
+                                                                        ages(y)
     {
-        
-    };
-#else
-    class Wine
-    {
+        strcpy(name, l);
+        calculateAges();
+    }
 
-    };
+    Wine::Wine(const char* l, int y) : name(new char(strlen(l))),
+                                       bottlingInfo(std::make_pair(ArrayIntT(y), ArrayIntT(y))),
+                                       ages(y)             
+    {
+        strcpy(name, l);
+    }
+
+    Wine::~Wine()
+    {
+        delete[] name;
+    }
+
+    void Wine::getBottles()
+    {
+        std::cout << "The wine " << name 
+                  << " has been prepared for " << bottlingInfo.first.size() << " years of bottling."
+                  << " Please, fill the info: " << std::endl;
+
+        size_t year;
+        size_t bottles;
+
+        for(size_t i = 0; i < bottlingInfo.first.size(); ++i)
+        {
+            std::cout << "Enter the year of bottling: ";
+            std::cin >> year;
+            std::cout << "Enter the count of bottles, were bottled in " << year << ": "; 
+            std::cin >> bottles;
+            bottlingInfo.first[i] = year;
+            bottlingInfo.second[i] = bottles;
+        }
+        calculateAges();
+        std::cout << std::endl;
+    }
+
+    void Wine::show() const
+    {
+        std::cout << "The wine " << name << " info: " << std::endl;
+
+        for(size_t i = 0; i < bottlingInfo.first.size(); ++i)
+        {
+            std::cout << "Year: " << bottlingInfo.first[i] << " | "
+                      << "Bottles: " << bottlingInfo.second[i] << " | "
+                      << "Age: " << ages[i] << std::endl;
+        }
+        std::cout << std::endl;
+    }
+
+    const char* Wine::label() const
+    {
+        return name;
+    }
+
+    size_t Wine::sum() const
+    {
+        return bottlingInfo.second.sum();
+    }
+
+    void Wine::calculateAges()
+    {
+        time_t theTime = time(NULL);
+        struct tm *aTime = localtime(&theTime);
+        int year = aTime->tm_year + 1900;
+
+        for(size_t i = 0; i < bottlingInfo.first.size(); ++i)
+        {
+            ages[i] = year - bottlingInfo.first[i];
+        }
+    }
+#else
+
 #endif
